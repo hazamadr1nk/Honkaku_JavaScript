6f0e81e4780ce7411664d7c3b0083bb3074dee2f
hazamadr1nk <110225244+hazamadr1nk@users.noreply.github.com>
2022-09-09 00:28:38 +0900

    chapter6を実装

--- scripts/handler.js
+++ scripts/handler.js
@@ -1,7 +1,18 @@
-//ページロードに実行されるイベントハンドラーを登録
-document.addEventListener("DOMContentLoaded",function(){
-    //ボタンクリック時に実行されるイベントリスナーを登録
-    document.getElementById("btn").addEventListener("click",function(){
-        window.alert("click button!");
-    },false);
+document.addEventListener("DOMContentLoaded", function(){
+    let Counter = function(elem){
+        this.count = 0;
+        this.elem = elem;
+        elem.addEventListener("click", () => {
+            this.count++;
+            this.show();
+        },false);
+    };
+
+    //カウンター情報を表示するためのshowメソッド
+    Counter.prototype.show = function(){
+        console.log(this.elem.id, "は", this.count, "回クリックされました。");
+    }
+
+    //button要素btnにカウンターを紐付け
+    let n = new Counter(document.getElementById("btn"));
 },false);
